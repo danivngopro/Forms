7007acad8235738785859c78d942273345a2eece
danivngopro <danivngopro@gmail.com>
2022-10-11 15:13:03 +0300

    adding interfaces

--- src/questions/questions.interface.ts
+++ src/questions/questions.interface.ts
@@ -1,5 +1,27 @@
-export interface question {
+export interface Survey {
   id?: string;
-  questionId: string;
-  members: Array<string>;
+  surveyName: string;
+  content: Array<Question>;
 }
+
+export interface Question {
+  id?: string;
+  questionName: string;
+  questionType: QuestionType;
+  answers?: Array<Answer>;
+}
+export interface Answer {
+  id?: string;
+  answer: string;
+}
+
+enum QuestionType {
+  shortAnswer = 'shortAnswer',
+  LongAnswer = 'LongAnswer',
+  radio = 'radio',
+  tickable = 'tickable',
+  select = 'select',
+  title = 'title',
+}
+
+
