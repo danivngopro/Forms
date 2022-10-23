bc140e3d2850937fa3788be6da1cca9799bc7192
Zoharadu <zohara.duenias@gmail.com>
2022-10-23 08:32:46 +0300

    navbar

--- forms/src/App.tsx
+++ forms/src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
 import './App.css';
+import Navbar from './components/form/navbar';
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import styled from '@emotion/styled';
+import {Cards} from './card';
+const StyledFab = styled(Fab)({
+  position: 'absolute',
+  zIndex: 1,
+  top: 670,
+  left: -1400,
+  right: 0,
+  margin: '0 auto',
+});
 
-function App() {
+export function App() {
   return (
-    <h1>hello my name is zohara</h1>
-  );
+    <>
+      <Navbar />
+      <Cards />
+      <StyledFab color="primary" aria-label="add">
+        <AddIcon />
+      </StyledFab>
+    </>
+  )
 }
 
 export default App;
