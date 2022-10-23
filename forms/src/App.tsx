31d5a0c8e635dea1926e1fd1eaaaeb50cb7a29f1
Zoharadu <zohara.duenias@gmail.com>
2022-10-23 14:49:59 +0300

    cards

--- forms/src/App.tsx
+++ forms/src/App.tsx
@@ -1,26 +1,13 @@
 import './App.css';
 import Navbar from './components/form/navbar';
-import AddIcon from '@mui/icons-material/Add';
-import Fab from '@mui/material/Fab';
-import styled from '@emotion/styled';
-import {Cards} from './card';
-const StyledFab = styled(Fab)({
-  position: 'absolute',
-  zIndex: 1,
-  top: 670,
-  left: -1400,
-  right: 0,
-  margin: '0 auto',
-});
+import { Cards } from './card';
 
 export function App() {
+
   return (
     <>
       <Navbar />
-      <Cards />
-      <StyledFab color="primary" aria-label="add">
-        <AddIcon />
-      </StyledFab>
+      <Cards/>
     </>
   )
 }
