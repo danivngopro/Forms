5f2842c892bb06ac3179f40774c7284f39135246
daninvngopro <danivngopro@gmail.com>
2022-11-04 16:28:09 +0200

    adding drag and drop functionality

--- forms/src/pages/SurveyCreationPage/SurveyCreationPage.tsx
+++ forms/src/pages/SurveyCreationPage/SurveyCreationPage.tsx
@@ -3,13 +3,23 @@ import SurveySection from "./components/form/SurveySection/SurveySection";
 import SurveyTitle from "./components/form/SurveyTitle/SurveyTitle";
 import "./SurveyCreationPage.scss";
 import plus from "../../assets/plus.svg";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 function SurveyCreationPage(props: { surveyName: string }) {
   const [sections, setSections] = useState(["a", "b", "c", "d"]);
 
   const addSection = () => {
     setSections([...sections, "e"]);
-  }
+  };
+
+  const handleOnDragEnd = (result: any) => {
+    if (!result.destination) return;
+    const items = Array.from(sections);
+    const [recordedItems] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, recordedItems);
+
+    setSections(items);
+  };
 
   return (
     <div className="survey-creation-page-container">
@@ -19,13 +29,33 @@ function SurveyCreationPage(props: { surveyName: string }) {
           <SurveyTitle surveyName={props.surveyName} />
         </div>
         <div className="survey-creation-page-section-container">
-          {sections.map((section, i) => (
-            <div key={i}>
-              <SurveySection text={section} />
-            </div>
-          ))}
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable droppableId="characters">
+              {(provided) => (
+                <ul className="survey-creation-page-secion-list" {...provided.droppableProps} ref={provided.innerRef}>
+                  {sections.map((section, i) => (
+                    <Draggable key={i} draggableId={`id${i}`} index={i}>
+                      {(provided) => (
+                        <li
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          ref={provided.innerRef}
+                        >
+                          <SurveySection text={section} />
+                        </li>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </ul>
+              )}
+            </Droppable>
+          </DragDropContext>
         </div>
-        <div className="survey-creation-page-plus-container" onClick={() => addSection()}>
+        <div
+          className="survey-creation-page-plus-container"
+          onClick={() => addSection()}
+        >
           <img
             src={plus}
             alt="add survey"
